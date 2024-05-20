ef17eebf44f050edd50bf207b6dc787d73a562f0
Daniel Osuoha <122853068+DanielOsuoha@users.noreply.github.com>
2024-05-20 11:32:34 +0000

    finished up with the asgi.py file

--- simple_chat/simple_chat/asgi.py
+++ simple_chat/simple_chat/asgi.py
@@ -1,23 +1,24 @@
-"""
-ASGI config for simple_chat project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
+# simple_chat/asgi.py
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.generic.websocket import WebsocketConsumer
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simple_chat.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", ".settings")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
-    "http": get_asgi_application(),
-    # Just HTTP for now. (We can add other protocols later.)
-}
-    )
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
